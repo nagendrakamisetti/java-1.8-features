9d1daaa606f68a81ff4b50c6b7ca56efe69c3c64
Nagendra Kamisetti <nagendrakamisetti@gmail.com>
2023-12-25 15:50:54 +0530

    refactoring

--- src/main/java/org/lambda/BasicLambda.java
+++ src/main/java/org/lambda/BasicLambda.java
@@ -4,20 +4,25 @@ package org.lambda;
 interface MyInterface {
     void myMethod();
 
+    // It can contain any number of default methods.
     default void method1() {
-        System.out.println("Hello");
+        System.out.println("Default method-1");
     }
+
+    default void method2() {
+        System.out.println("Default method-2");
+    }
+
+    // It can contain any number of Object class methods.
+    int hashCode();
+    String toString();
+    boolean equals(Object obj);
+
 }
 
 public class BasicLambda {
     public static void main(String[] args) {
        MyInterface myInterface = () -> System.out.println("Inside myMethod");
-       //myInterface.myMethod();
-        BasicLambda a = new BasicLambda();
-        a.testMethod(myInterface);
-    }
-
-    private void testMethod(MyInterface myInterface) {
-        myInterface.myMethod();
+       myInterface.myMethod();
     }
 }
